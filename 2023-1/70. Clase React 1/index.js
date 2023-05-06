3b3419c4c56a6341310b16af18c0190c7e218db0
ProfL3301 <ProfL3301@lab.aca>
2023-05-06 13:33:58 -0500

    adding react class with next

---
+++ 2023-1/70. Clase React 1/index.js
@@ -0,0 +1,20 @@
+const arr = ["Juan", "Carlos", "Pedro"]
+
+const Imprimir2 = () => {
+    arr.forEach(e => {
+        console.debug(e);
+        
+    });
+}
+
+function Imprimir () {
+    for (i = 0; i < 5; i++) {
+        console.debug(i)
+    }
+}
+
+Imprimir();
+Imprimir2();
+
+
+
---
+++ 2023-1/70. Clase React 1/my-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\page.js": [
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\page.module.css"
+    ],
+    "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\layout.js": [
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\page": [
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\page.module.css",
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\src\\app\\globals.css",
+      "C:\\Temp\\prograweb202301\\clase react 1\\my-app\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
